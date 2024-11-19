838afe7b85a8fb8173e5091ee85bef3819630d0b
PLChandrasekara <139640043+PLChandrasekara@users.noreply.github.com>
2024-11-19 13:16:37 +0000

    create app folder

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import StudentsDb from "./StudentsDb";
+import FontSizeSelector from "./components/FontSizeSelector";
+import Table from "./components/Table";
+import Profile from "./components/Profile";
+import './compo.css';
+const App = () => {
+  const [students] = useState(StudentsDb);
+  const [selectedStudent, setSelectedStudent] = useState(students[0]);
+  const [fontSize, setFontSize] = useState("16px");
 
-function App() {
+  const selectStudent = (index) => {
+    setSelectedStudent(students[index]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", fontSize }}>
+      <div style={{ flex: 1 }}>
+        <FontSizeSelector fontSize={fontSize} setFontSize={setFontSize} />
+        <Table students={students} selectStudent={selectStudent} />
+      </div>
+      <div style={{ flex: 1, marginLeft: "20px" }}>
+        <Profile student={selectedStudent} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
