d0afbff0bb2182e5cd0aa555676539b40c4c4298
PLChandrasekara <139640043+PLChandrasekara@users.noreply.github.com>
2024-11-19 09:31:52 +0000

    create table

---
+++ myapp/src/components/Table.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Table = ({ students, selectStudent }) => {
+    return (
+        <table border="1" style={{ width: "100%" }}>
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Course</th>
+                    <th>Country</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {students.map((student, index) => (
+                    <tr key={index}>
+                        <td>{student.firstName}</td>
+                        <td>{student.lastName}</td>
+                        <td>{student.course}</td>
+                        <td>{student.country}</td>
+                        <td>
+                            <button onClick={() => selectStudent(index)}>View Profile</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default Table;
